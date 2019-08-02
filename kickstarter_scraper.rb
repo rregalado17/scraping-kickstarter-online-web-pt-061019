3089c049e9a21201f32378eb58ff7e6fd59ca5b0
Robinson Regalado <rregalado17@gmail.com>
2019-08-02 04:27:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
  
   kickstarter = Nokogiri::HTML(html)
-  binding.pry 
+   
   hash = {
     
   }
@@ -40,9 +40,3 @@ end
 
 
 
-
-
-
-
-
-  create_project_hash
